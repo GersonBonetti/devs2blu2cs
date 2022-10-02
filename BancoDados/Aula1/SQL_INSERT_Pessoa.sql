f1987592925a2eadc78d837fa75ddede31ec4cb9
Gérson Bonetti <87243693+GersonBonetti@users.noreply.github.com>
2022-10-02 09:51:13 -0300

    Pasta de projetos atualizada
    Atualização dos últimos projetos desenvolvidos no curso.

---
+++ BancoDados/Aula1/SQL_INSERT_Pessoa.sql
@@ -0,0 +1,9 @@
+INSERT INTO pessoa (nome, datanascimento, idade, status)
+VALUES ('Alexandre Schwamke','2000-01-01',22,1),
+		('Ana Moreira','2000-01-01',22,1),
+		('Bernardo Miguel','2000-01-01',22,1),
+        ('Carlos Xavier','2000-01-01',22,1),
+        ('Cora Amorim','2000-01-01',22,1),
+        ('Eduardo Cestari','2000-01-01',22,1),
+        ('Yasmin Alves','2000-01-01',22,1)
+;
